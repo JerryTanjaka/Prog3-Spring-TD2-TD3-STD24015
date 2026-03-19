293b721daed531477d3f2df1ea1f19d75fbf9115
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-03-19 11:24:54 +0300

    feat : add Student model

--- src/main/java/hei/springboottd/entity/Student.java
+++ src/main/java/hei/springboottd/entity/Student.java
@@ -1,10 +1,48 @@
 package hei.springboottd.entity;
 
-@
+
 public class Student {
     private String reference;
     private String FirstName;
     private String LastName;
     private int name;
 
+    public Student(String reference, String firstName, String lastName, int name) {
+        this.reference = reference;
+        FirstName = firstName;
+        LastName = lastName;
+        this.name = name;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
 }
