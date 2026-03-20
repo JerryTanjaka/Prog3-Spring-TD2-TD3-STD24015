1af7841e587bc77af7f4e086d1615087497757c1
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-03-20 15:49:41 +0300

    feat: implement POST /students with in-memory storage and 201 response

--- src/main/java/hei/springboottd/controller/StudentController.java
+++ src/main/java/hei/springboottd/controller/StudentController.java
@@ -2,6 +2,8 @@ package hei.springboottd.controller;
 
 import hei.springboottd.entity.Student;
 import hei.springboottd.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +20,18 @@ public class StudentController {
         this.studentService = studentService;
     }
     @PostMapping("/students")
-    public List<Student> createStudent(@RequestBody List<Student> newStudents) {
-        return studentService.addStudent(newStudents);
+    public ResponseEntity<List<Student>> addStudents(@RequestBody List<Student> students) {
+        try {
+            List<Student> allStudents = studentService.addStudent(students);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(allStudents);
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .build();
+        }
     }
     @GetMapping("/students")
     public String getStudents(@RequestHeader("Accept") String acceptHeader ) {
