3cd20627a495d8025a96cc0ea5e2ab1da554838f
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2026-03-26 10:43:51 +0300

    feat : add student info on input error message to know what stuend field does not match the validator

--- src/main/java/hei/springboottd/validator/StudentValidator.java
+++ src/main/java/hei/springboottd/validator/StudentValidator.java
@@ -8,16 +8,16 @@ import java.util.List;
 public class StudentValidator {
     public void isStudentFieldvalid(Student student){
         if(student.getReference() == null){
-            throw new RuntimeException("reference field is not valid");
+            throw new RuntimeException("reference field is not valid "+student.getFirstName()+student.getLastName());
         }
         if (student.getFirstName()== null){
-            throw new RuntimeException("firstName field is not valid");
+            throw new RuntimeException("firstName field is not valid "+student.getReference());
         }
         if (student.getLastName()== null){
-            throw new RuntimeException("lastName field is not valid");
+            throw new RuntimeException("lastName field is not valid "+student.getReference());
         }
         if (student.getAge() <= 0){
-            throw new RuntimeException("age field is not valid");
+            throw new RuntimeException("age field is not valid "+student.getReference());
         }
     }
 public void isStudentsListvalid(List<Student> students){
